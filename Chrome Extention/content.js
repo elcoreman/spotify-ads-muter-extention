d4a442fc603e768be3553cdfdcd495f29a61d6ad
Mohammad Farshadpour <farshadpour@hotmail.com>
2021-01-16 18:25:17 +0330

    Hide Unnecessary elements

--- Chrome Extention/content.js
+++ Chrome Extention/content.js
@@ -6,6 +6,7 @@
       this.speakerSelector = props.speakerSelector;
       this.musicLengthSelector = props.musicLengthSelector;
       this.maxAdsLength = props.maxAdsLength;
+      this.shouldHideSelectors = props.shouldHideSelectors;
     }
     get isMuted() {
       const speaker = document.querySelector(this.speakerSelector);
@@ -44,15 +45,33 @@
     adIsPlaying() {
       return this.musicLength <= this.maxAdsLength;
     }
+    hideElements() {
+      document
+        .querySelectorAll(this.shouldHideSelectors.join(","))
+        .forEach((elem) => (elem.style.display = "none"));
+    }
   }
 
-  const site = new Site({
+  const init = {
+    maxAdsLength: 62,
     speakerSelector:
       "#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.now-playing-bar__right > div > div.volume-bar > button",
     musicLengthSelector:
       "#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.now-playing-bar__center > div > div.playback-bar > div:nth-child(3)",
-    maxAdsLength: 62,
-  });
+    shouldHideSelectors: [
+      "#main > div > div.Root__top-container > div.Root__main-view > div:nth-child(3) > div",
+      "#main > div > div.Root__top-container > nav > div:first-of-type > div:nth-child(4)",
+      "#main > div > div.Root__top-container > div.Root__top-bar > header > button",
+    ],
+  };
 
-  setInterval(() => (site.adIsPlaying() ? site.mute() : site.unMute()), 3e3);
+  const site = new Site(init);
+
+  const check = () => {
+    site.adIsPlaying() ? site.mute() : site.unMute();
+    site.hideElements();
+  };
+
+  setInterval(check, 3e3);
+  check();
 })();
