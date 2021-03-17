ffa48b32248e2c70bd16fb4a8d1204179e46f0a1
Mohammad Farshadpour <farshadpour@hotmail.com>
2021-03-18 00:33:37 +0330

    Work on different languages

--- Chrome Extention/content.js
+++ Chrome Extention/content.js
@@ -1,75 +1,73 @@
 (function () {
-  console.log(`"Spotify Ads Muter" succussfully injected to Spotify`);
+	console.log(`"Spotify ads muter" succussfully injected to Page`)
 
-  class Site {
-    constructor(props) {
-      this.speakerSelector = props.speakerSelector;
-      this.musicLengthSelector = props.musicLengthSelector;
-      this.maxAdsLength = props.maxAdsLength;
-      this.shouldHideSelectors = props.shouldHideSelectors;
-    }
-    get isMuted() {
-      const speaker = document.querySelector(this.speakerSelector);
-      return (
-        speaker?.getAttribute("aria-label") &&
-        speaker.getAttribute("aria-label") != "Mute"
-      );
-    }
-    mute() {
-      const speaker = document.querySelector(this.speakerSelector);
-      return this.isMuted ? false : speaker?.click();
-    }
-    unMute() {
-      const speaker = document.querySelector(this.speakerSelector);
-      return this.isMuted ? speaker?.click() : false;
-    }
-    get musicLength() {
-      let length = 0;
-      try {
-        length = document
-          .querySelector(this.musicLengthSelector)
-          .innerText.split(":")
-          .map((v) => Number(v)) // convert each string value to numbers
-          .map((v, i, arr) => {
-            const mul = 60 ** (arr.length - i - 1);
-            return v * mul;
-          }) // convert each value to seconds
-          .reduce((a, c) => a + c); // Sum up seconds
-      } catch (err) {
-        return length;
-      }
-      return length;
-    }
-    adIsPlaying() {
-      return this.musicLength <= this.maxAdsLength;
-    }
-    hideElements() {
-      return document
-        .querySelectorAll(this.shouldHideSelectors.join(","))
-        .forEach((elem) => (elem.style.display = "none"));
-    }
-  }
+	class Site {
+		constructor(props) {
+			this.props = props
+		}
+		isMuted() {
+			const speakerSvgPath = document.querySelector(this.props.selectors.speaker + " > svg > path")
+			return speakerSvgPath?.getAttribute("d")?.includes("L12")
+		}
+		mute() {
+			const speaker = document.querySelector(this.props.selectors.speaker)
+			return !this.isMuted() && speaker?.click()
+		}
+		unMute() {
+			const speaker = document.querySelector(this.props.selectors.speaker)
+			return this.isMuted() && speaker?.click()
+		}
+		musicLength() {
+			let length = 0
+			try {
+				length = document
+					.querySelector(this.props.selectors.musicLength)
+					.innerText.split(":")
+					.map(v => Number(v)) // convert each string value to numbers
+					.map((v, i, arr) => {
+						const mul = 60 ** (arr.length - i - 1)
+						return v * mul
+					}) // convert each value to seconds
+					.reduce((a, c) => a + c) // Sum up seconds
+			} catch (err) {
+				return length
+			}
+			return length
+		}
+		isAdPlaying() {
+			return this.musicLength() <= this.props.ads.maxLength
+		}
+		hideElements() {
+			return document
+				.querySelectorAll(this.props.selectors.shouldHide.join(","))
+				.forEach(elem => (elem.style.display = "none"))
+		}
+	}
 
-  const init = {
-    maxAdsLength: 62,
-    speakerSelector:
-      "#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.now-playing-bar__right > div > div.volume-bar > button",
-    musicLengthSelector:
-      "#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.now-playing-bar__center > div > div.playback-bar > div:nth-child(3)",
-    shouldHideSelectors: [
-      "#main > div > div.Root__top-container > div.Root__main-view > div:nth-child(3) > div",
-      "#main > div > div.Root__top-container > nav > div:first-of-type > div:nth-child(4)",
-      "#main > div > div.Root__top-container > div.Root__top-bar > header > button",
-    ],
-  };
+	const config = {
+		ads: {
+			maxLength: 1.1 * 60,
+		},
+		selectors: {
+			speaker:
+				"#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.now-playing-bar__right > div > div.volume-bar > button",
+			musicLength:
+				"#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.now-playing-bar__center > div > div.playback-bar > div:nth-child(3)",
+			shouldHide: [
+				"#main > div > div.Root__top-container > div.Root__main-view > div:nth-child(3) > div",
+				"#main > div > div.Root__top-container > nav > div:first-of-type > div:last-of-type > div:first-of-type",
+				"#main > div > div.Root__top-container > div.Root__top-bar > header > button",
+			],
+		},
+	}
 
-  const site = new Site(init);
+	const site = new Site(config)
 
-  const check = () => {
-    site.adIsPlaying() ? site.mute() : site.unMute();
-    site.hideElements();
-  };
+	const check = () => {
+		site.isAdPlaying() ? site.mute() : site.unMute()
+		site.hideElements()
+	}
 
-  setInterval(check, 3e3);
-  check();
-})();
+	setInterval(check, 3e3)
+	check()
+})()
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Intro
-Mute Spotify sound whenever ads played and unmute when finished
+Mute or hide all kind of Ads on open.spotify.com such as sounds, graphics and links.
 
 ## How it works
-It just check music player's max length every 3 seconds, if length is equal or less than 62, detect sound as ads and mute site otherwise unmute it.
+To mute sound ads it just check music player's max length every 3 seconds, if length is equal or less than 1.1 minutes, detect sound as ads and mute site otherwise unmute it.
+To hide graphics and links ads it has list of selectors to hide them every 3 seconds.
 
 ## Usage
 Just Drag & Drop extention folder to your browser extention list
